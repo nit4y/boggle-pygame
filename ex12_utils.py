c1075f3fea9c67768b1b7591310b7747a0f28da0
Boaz Shimon <boaz.shimon@yahoo.com>
2022-01-02 20:00:49 +0200

    added prototype find_length_n_words!!! N_WORDS!!!!

--- ex12_utils.py
+++ ex12_utils.py
@@ -43,6 +43,11 @@ def _does_word_have_future(word, words) -> bool:
     pass
 
 
+############################################################
+
+
+
+
 def find_length_n_paths(n, board, words):
     words_with_n_length = []
     for word in words:
@@ -77,20 +82,53 @@ def _find_length_n_paths_core(n, board, cur_location, used_locations, word, word
 
 
 
-
+#############################################################
 
 
 def find_length_n_words(n, board, words):
-    pass
+    words_with_n_length = []
+    for word in words:
+        if len(word) == n:
+            words_with_n_length.append(word)
+
+    correct_paths = [] #initalizing list we will return
+    for y in range(4):
+        for x in range(4):
+            _find_length_n_words_core(n,board,(y,x),[],"",words_with_n_length,correct_paths)
+    return correct_paths
+
+
+def _find_length_n_words_core(n, board, cur_location, used_locations, word, words, correct_paths):
+    if len(word) >= n:
+        if word in words:
+            if used_locations not in correct_paths:
+                correct_paths.append(used_locations)
+        return
+
+    used_locations.append(cur_location)
+    if not _are_locations_legal(used_locations):
+        return
+
+    y, x = cur_location[0], cur_location[1]
+    word += board[y][x]
+
+    for direction in DIRECTIONS:
+        new_location = ( y + direction[0], x + direction[1])
+        _find_length_n_words_core(n,board,new_location,used_locations[:], word[:], words, correct_paths)
+
+
+
 
 def max_score_paths(board, words):
     pass
 
+
+
 if __name__ == "__main__":
     board = [["a","b","c","d"],["e","f","g","h"],["i","j","k","l"],["m","n","o","p"]]
-    words = ["abc","def","ghif","ab"]
+    words = ["abc","def","ghif","ab","efj","mno"]
     #print(is_valid_path([["a","b","c"],["d","e","f"],["g","h","i"]], [(0,0),(0,1),(0,2)], ["ccc"]))
-    print(find_length_n_paths(3,board,words))
+    print(find_length_n_words(3,board,words))
    # print(_are_locations_legal( [(0,0),(0,1),(0,0)])) #FALSE
     #print(_are_locations_legal([(0, 0), (0, 1), (0, 2)])) #TRUE
     #print(_are_locations_legal([(0, 0), (0, 1), (-1,1)]))  # FALSE
