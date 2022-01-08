4e324f6e2302b3a37082736acd56647c506b635e
Nitay Shoshana <nitay2020@gmail.com>
2022-01-09 01:25:59 +0200

    add: transition from main menu to game window

--- consts.py
+++ consts.py
@@ -1,2 +1,9 @@
 
 DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1), (1,1), (-1,1), (1,-1), (-1,-1)]
+
+
+# Color pallete
+PRIMARY = "orange"
+SECONDARY = "wheat"
+REGULAR_COLOR = 'lightgray'
+MAIN_FONT = "Courier New"
--- ui.py
+++ ui.py
@@ -1,9 +1,6 @@
-import tkinter
-from tkinter import *
-#import mp3play
-import sys
-
-REGULAR_COLOR = 'lightgray'
+import tkinter as tk
+from tkinter import CENTER, TOP, BOTH, NW
+from consts import PRIMARY, SECONDARY, REGULAR_COLOR, MAIN_FONT 
 
 
 def default():
@@ -11,111 +8,137 @@ def default():
 
 class UserInterface(object):
     def __init__(self) -> None:
-        root = Tk()
+        root = tk.Tk()
         self._main_window = root
-        root.title = "Boggle"
+        root.title("Boggle")
         root.iconbitmap('assets/icon.ico')
-        frame = Frame(root, bg=REGULAR_COLOR,
-                                      highlightbackground=REGULAR_COLOR,
-                                      highlightthickness=5)
+        frame = tk.Frame(root)
         self._main_frame = frame
+        frame.grid(row=0, column=0, sticky="nsew")
+        
+        bg = tk.PhotoImage(file="assets/resized.png")
+        self._main_menu_canvas = tk.Canvas(frame, height=600, width=600, bd=0, highlightthickness=1)
+        self._main_menu_canvas.create_image(0, 0, image=bg, anchor=NW, tags="IMG")
+        self._main_menu_canvas.pack(side=TOP, fill=BOTH, expand=True)
+
         self.game_screen_display()
-
-        frame.pack(side=TOP, fill=BOTH, expand=True)
-        bg = PhotoImage(file="assets/resized.png")
-        self.display = Canvas(frame, height=600, width=600, bd=0, highlightthickness=1)
-        self.display.create_image(0, 0, image=bg, anchor=NW, tags="IMG")
-
-        self.display.pack(side=TOP, fill=BOTH, expand=True)
         
         self._positioning()
+        self._create_buttons(self._main_menu_canvas,{
+            1: {
+                "text": "Start",
+                "command": self._show_game_frame,
+                "relx" : 0.5,
+                "rely": 0.5
+            },
+            2: {
+                "text": "Exit",
+                "command": self.exit,
+                "relx" : 0.5,
+                "rely": 0.6
+            },
+            3: {
+                "text": "?",
+                "command": default,
+                "relx" : 0.95,
+                "rely": 0.95,
+                "width": 50,
+                "height": 30
+            },
+            
+        })
+        
+        self._show_frame(self._main_frame)
         root.mainloop()
 
-    def game_screen_display(self):
-        root = self._main_window
-        game_display_frame = Frame(root, bg=REGULAR_COLOR,
-                               highlightbackground=REGULAR_COLOR,
-                               highlightthickness=5)
-        self.side_display_maker(game_display_frame)
-        self.current_word_display(game_display_frame)
-
-        self.four_by_four_maker(game_display_frame)
-        game_display_frame.pack()
-
-    def side_display_maker(self, frame):
-        root = self._main_window
-        frame_for_side_bar = Frame(frame, bg=REGULAR_COLOR,
-                               highlightbackground="black",
-                               highlightthickness=5)
-        frame_score = Frame(frame_for_side_bar, bg=REGULAR_COLOR,
-                               highlightbackground=REGULAR_COLOR,
-                               highlightthickness=5)
-        score_title = tkinter.Label(frame_score, text = "SCORE:", font = ("Courier", 30))
-        score_title.pack()
-        score_actual = tkinter.Label(frame_score, text = "9999", font = ("Courier", 30))
-        score_actual.pack()
-        frame_score.pack()
-        ######################
-        frame_time = Frame(frame_for_side_bar, bg=REGULAR_COLOR,
-                            highlightbackground=REGULAR_COLOR,
-                            highlightthickness=5)
-        time_title = tkinter.Label(frame_time, text="TIME:",
-                                    font=("Courier", 30))
-        time_title.pack()
-        time_actual = tkinter.Label(frame_time, text="9999",
-                                     font=("Courier", 30))
-        time_actual.pack()
-        frame_time.pack()
-        #####################
-        frame_found_words = Frame(frame_for_side_bar, bg=REGULAR_COLOR,
-                           highlightbackground=REGULAR_COLOR,
-                           highlightthickness=5)
-        words_title = tkinter.Label(frame_found_words, text="WORDS:",
-                                   font=("Courier", 30))
-        words_title.pack()
-        words_actual = tkinter.Label(frame_found_words, text="test",
-                                    font=("Courier", 30))
-        words_actual.pack()
-        frame_found_words.pack()
-        frame_for_side_bar.pack(side = tkinter.LEFT)
-
-
-    def current_word_display(self,frame):
-        root = self._main_window
-        frame_display = Frame(frame,  bg="wheat",
-                               highlightbackground="wheat",
-                               highlightthickness=5)
-        label = tkinter.Label(frame_display, text = "test", font = ("Courier", 50))
-        label.pack()
-        frame_display.pack(side = tkinter.TOP)
-
-    def four_by_four_maker(self,frame) -> None:
-        root = self._main_window
-        #f = mp3play.load('boom.mp3'); play = lambda: f.play()
-        for i in range(4):
-            frame_grid = Frame(frame, bg="black",
-                               highlightbackground="wheat",
-                               highlightthickness=5)
-            for j in range(4):
-                b = tkinter.Button(frame_grid, text= str(i)+","+str(j), bg = "orange",font=("Courier", 30))#, command = play)
-                b.pack()
-            frame_grid.pack(side = tkinter.LEFT)
 
     def run(self) -> None:
         self._main_window.mainloop()
     
-    def _create_buttons(self):
-        buttons = {
-            {
-                "text": "Start",
-                "command": default(),
-            }
-        }
+    def exit(self) -> None :
+        self._main_window.destroy()
 
-        for b in buttons:
-            button = Button(self._main_frame, text=b["text"])
-            button.pack(side=TOP, fill=BOTH, expand=True)
-            
+    def _show_game_frame(self):
+        self._show_frame(self._game_display_frame)
+        
+    def _create_buttons(self, parent, buttons):
+        for _, b in buttons.items():
+            button = tk.Button(parent, background = PRIMARY, text = b["text"], font=(MAIN_FONT, 18), command = b["command"])
+            button.place(relx = b["relx"], rely = b["rely"], anchor = CENTER, width = b.get("width", 100), height = b.get("height", 50))
+    
+    def _show_frame(self, frame):
+        frame.tkraise()
+        
+    def game_screen_display(self):
+        root = self._main_window
+        game_display_frame = tk.Frame(root, bg=REGULAR_COLOR,
+                               highlightbackground=REGULAR_COLOR,
+                               highlightthickness=5, width=600, height=600)
+        self.side_display_maker(game_display_frame)
+        self.current_word_display(game_display_frame)
+
+        self.four_by_four_maker(game_display_frame)
+        game_display_frame.grid(row=0, column=0, sticky="nsew")
+        self._game_display_frame = game_display_frame
+
+    def side_display_maker(self, frame):
+        root = self._main_window
+        frame_for_side_bar = tk.Frame(frame, bg=REGULAR_COLOR,
+                               highlightbackground="black",
+                               highlightthickness=5)
+        frame_score = tk.Frame(frame_for_side_bar, bg=REGULAR_COLOR,
+                               highlightbackground=REGULAR_COLOR,
+                               highlightthickness=5)
+        score_title = tk.Label(frame_score, text = "SCORE:", font = (MAIN_FONT, 30))
+        score_title.pack()
+        score_actual = tk.Label(frame_score, text = "9999", font = (MAIN_FONT, 30))
+        score_actual.pack()
+        frame_score.pack()
+        ######################
+        frame_time = tk.Frame(frame_for_side_bar, bg=REGULAR_COLOR,
+                            highlightbackground=REGULAR_COLOR,
+                            highlightthickness=5)
+        time_title = tk.Label(frame_time, text="TIME:",
+                                    font=(MAIN_FONT, 30))
+        time_title.pack()
+        time_actual = tk.Label(frame_time, text="9999",
+                                     font=(MAIN_FONT, 30))
+        time_actual.pack()
+        frame_time.pack()
+        #####################
+        frame_found_words = tk.Frame(frame_for_side_bar, bg=REGULAR_COLOR,
+                           highlightbackground=REGULAR_COLOR,
+                           highlightthickness=5)
+        words_title = tk.Label(frame_found_words, text="WORDS:",
+                                   font=(MAIN_FONT, 30))
+        words_title.pack()
+        words_actual = tk.Label(frame_found_words, text="test",
+                                    font=(MAIN_FONT, 30))
+        words_actual.pack()
+        frame_found_words.pack()
+        frame_for_side_bar.pack(side = tk.LEFT)
+
+
+    def current_word_display(self,frame):
+        root = self._main_window
+        frame_display = tk.Frame(frame,  bg="wheat",
+                               highlightbackground="wheat",
+                               highlightthickness=5)
+        label = tk.Label(frame_display, text = "test", font = (MAIN_FONT, 50))
+        label.pack()
+        frame_display.pack(side = tk.TOP)
+
+
+    def four_by_four_maker(self, frame) -> None:
+        for i in range(4):
+            frame_grid = tk.Frame(frame, bg="black",
+                               highlightbackground="wheat",
+                               highlightthickness=5)
+            for j in range(4):
+                b = tk.Button(frame_grid, text= str(i)+","+str(j), bg = "orange",font=(MAIN_FONT, 30))
+                b.pack()
+            frame_grid.pack(side = tk.LEFT)
+
 
     def _positioning(self):
         root = self._main_window
@@ -134,8 +157,5 @@ class UserInterface(object):
 
 
 if __name__ == "__main__":
-    print(sys.path)
-
     ui = UserInterface()
-
     #ui.run()
